987b97aa0bcf926449b3685e5ce2a0adc7b67280
Peiran Liu <pl2774@columbia.edu>
2020-10-15 00:44:36 +0000

    create model and class squirrel

--- SquirrelApp/models.py
+++ SquirrelApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+#class Map(models.Model):
+  #return "hello"
+
+
+class Squirrel(models.Model):
+    UniqueSquirrelID=models.CharField(
+            max_length=100,
+            help_text='Unique Squirrel ID',
+        )
+    Date=models.DateField(
+            help_text='Date',
+        )
+    LinktoUnique=models.URLField(
+            max_length=300,
+            unique=True,
+        )
+
+    #def get_squirrel(self):
+        #return self.Squirrel 
