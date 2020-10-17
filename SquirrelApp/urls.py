470fe6e9442c99e9af8c2af32b7cbecd08f73eae
Peiran Liu <pl2774@columbia.edu>
2020-10-17 19:18:34 +0000

    update map related view,url,html

--- SquirrelApp/urls.py
+++ SquirrelApp/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
         path('sightings', views.index, name='index'),
         path('sightings/<str:sighting_id>', views.sighting_details, name='detail'),
         path('sightings/add', views.add_request, name='add'),
-
+        path('map',views.map,name='map'),
         ]
