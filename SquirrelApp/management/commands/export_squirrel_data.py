d443dd10cf6a6766871e4efcf342abf283ac10d5
Peiran Liu <pl2774@columbia.edu>
2020-10-22 21:33:30 +0000

    export data WORK!

--- SquirrelApp/management/commands/export_squirrel_data.py
+++ SquirrelApp/management/commands/export_squirrel_data.py
@@ -1,22 +1,73 @@
-#place holding for the export command
 import csv
-from tools import DynamicCommand
-from SquirrelApp.model import Sighting as sighting
-from django.core.management.base import BaseCommand,CommandError
+
+from django.core.management.base import BaseCommand
+from SquirrelApp.models import Sighting
+
 
 class Command(BaseCommand):
-    help='Exporting squirrels information to csv file.'
 
     def add_arguments(self,parser):
-        #parser.add_argument('model',help='SquirrelApp.model')
-        parser.add_argument('outfile',type=str,help='save path, like</path/to/outfile.csv>')
+        parser.add_argument('path')
 
-    def handle(self,*args,**options):
-        from django.apps import app
-        app_name=SquirrelApp
-        model=sighting
-        field_names=[f.name for f in model._meta.fields]
-        wrtier=csv.writer(opent(options['outfile'[0],'w'),quoting=csv.QUOTE_ALL,delimiter=-',')
-        writer.writerow(field_names)
-        for item in model.objects.all():
-            writer.writerow([str(getattr(instance,f)) for f in field_names]
+    def handle(self, *args, **options):
+        with open(options['path'][0],'w') as fp:
+            export_writer = csv.DictWriter(
+                    fp,
+                    delimiter=',',
+                    fieldnames=[
+                        'X',
+                        'Y',
+                        'Unique Squirrel ID',
+                        'Shift',
+                        'Date',
+                        'Age',
+                        'Primary Fur Color',
+                        'Location',
+                        'Specific Location',
+                        'Running',
+                        'Chasing',
+                        'Climbing',
+                        'Eating',
+                        'Foraging',
+                        'Other Activities',
+                        'Kuks',
+                        'Quaas',
+                        'Moans',
+			'Tail flags',
+                        'Tail twitches',
+                        'Approaches',
+                        'Indifferent',
+                        'Runs from',
+                        ]
+                    )
+            export_writer.writeheader()
+
+            for row in Sighting.objects.all():
+                export_writer.writerow({
+                    'X':row.Longitude,
+                    'Y':row.Latitude,
+                    'Unique Squirrel ID':row.UniqueSquirrelID,
+                    'Shift':row.Shift,
+                    'Date':row.Date,
+                    'Age':row.Age,
+                    'Primary Fur Color':row.PrimaryFurColor,
+                    'Location':row.Location,
+                    'Specific Location':row.SpecificLocation,
+                    'Running':row.Running,
+                    'Chasing':row.Chasing,
+                    'Climbing':row.Climbing,
+                    'Eating':row.Eating,
+                    'Foraging':row.Foraging,
+                    'Other Activities':row.OtherActivities,
+                    'Kuks':row.Kuks,
+                    'Quaas':row.Quaas,
+                    'Moans':row.Moans,
+                    'Tail flags':row.TailFlags,
+                    'Tail twitches':row.TailTwitches,
+                    'Approaches':row.Approaches,
+		    'Indifferent':row.Indifferent,
+                    'Runs from':row.RunsFrom,
+                    })
+
+            msg=f'Export Succeed!'
+            self.stdout.write(self.style.SUCCESS(msg))
